566f5de94befe304d1b71a868d2f65f06bccc8f4
Camilalb <milafviana@gmail.com>
2021-08-15 17:02:01 -0300

    Início do Projeto

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+function vanyar (){
+    location.replace("vanyar1.html")
+}
+
+function noldor (){
+  location.replace("noldor.html")
+}
+
+function teleri (){
+  location.replace("teleri.html")
+}
