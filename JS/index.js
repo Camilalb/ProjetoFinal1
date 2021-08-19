37102163c2dbad590d579e4a6125010b5801e2ad
Camilalb <milafviana@gmail.com>
2021-08-19 16:51:46 -0300

    Ajuste do Personagem Noldor

--- JS/index.js
+++ JS/index.js
@@ -9,17 +9,3 @@ function noldor (){
 function teleri (){
   location.href = "Personagem3/teleri1.html"
 }
-
-// function escreverDevagar(elemento) {
-//   const textoArray = elemento.innerHTML.split('');
-//   elemento.innerHTML = '';
-//   textoArray.forEach(letra,i) => {
-//     setTimeout(function(){
-//       elemento.innerHTML += letra
-//     }, 75 * i);
-//   });
-  
-// }
-
-// const titulo = Document.querySelector ('h1')
-// typeWriter (titulo);
